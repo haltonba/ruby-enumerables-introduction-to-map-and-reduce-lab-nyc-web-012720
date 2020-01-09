f7b98f9ef59f2cb16940dcfa38e94f8bd4e09e6f
Halton Bagley <haltonba94@gmail.com>
2020-01-09 17:00:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,6 +49,16 @@ def reduce_to_all_true (source_array)
   true
 end
 
+def reduce_to_any_true (source_array)
+  counter = 0
+  while counter < source_array.length do
+    if source_array[counter]
+      return true
+    end
+    counter += 1
+  end
+  false
+end
 
 
 
