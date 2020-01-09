8c95bc2dc77df97dfddbef905635f841a42f6083
Halton Bagley <haltonba94@gmail.com>
2020-01-09 16:31:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ end
 
 def reduce_to_total (source_array, starting_point = 100)
   counter = 0
-  total = starting_point 
+  total = starting_point || total = 0
   while counter < source_array.length do
     total += source_array[counter]
     counter += 1
