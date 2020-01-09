fade75837281214c3f76d88071cd42aa844417c6
Halton Bagley <haltonba94@gmail.com>
2020-01-09 16:06:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,12 @@ end
 def map_to_no_change (array)
   array
 end
+
+def map_to_double (array)
+  counter = 0
+  while counter < array.length do
+    array[counter] *= 2
+    counter += 1  
+  end
+  array
+end
